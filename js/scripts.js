27dda07fc2713b1cb2ab4e3320f7e9f91009c29b
amarcato933 <a.marcato93@gmail.com>
2021-09-01 10:13:20 +0200

    Added api key maps

--- js/scripts.js
+++ js/scripts.js
@@ -241,7 +241,7 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 40.116241, lng: 18.165005};
+    var location = {lat: 40.115611, lng: 18.165472};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: location,
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 40.116241, lng: 18.165005};
+    var la_fiesta = {lat: 40.115611, lng: 18.165472};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
